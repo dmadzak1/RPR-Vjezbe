53b78f0e886d7c6066e59e3753beb50d3f8c641e
dmadzak1 <dmadzak1@etf.unsa.ba>
2023-12-25 23:18:31 +0100

    h

--- lv4-z1/src/main/java/ba/unsa/etf/rpr/LaptopDaoJSONFile.java
+++ lv4-z1/src/main/java/ba/unsa/etf/rpr/LaptopDaoJSONFile.java
@@ -1,5 +1,8 @@
 package ba.unsa.etf.rpr;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+
 import java.io.*;
 import java.io.IOException;
 import java.lang.reflect.Array;
@@ -21,21 +24,31 @@ public class LaptopDaoJSONFile implements LaptopDao{
 
     @Override
     public void dodajLaptopUFile(Laptop laptop) throws IOException {
-
+        laptopi.add(laptop);
+        JsonMapper mapper=new JsonMapper();
+        String l=mapper.writeValueAsString(laptop);
+        FileOutputStream tok=new FileOutputStream(file);
+        tok.write(l.getBytes());
+        tok.close();
     }
 
     @Override
     public Laptop getLaptop(String procesor) {
-        return null;
+        for(Laptop l:laptopi){
+            if(l.getProcesor().equals(procesor)) return l;
+        }
+        throw new NemaProcesoraException("Ne postoji laptop sa ovim procesorom!");
     }
 
     @Override
     public void napuniListu(ArrayList<Laptop> laptopi) {
-
+        this.laptopi.addAll(laptopi);
     }
 
     @Override
     public ArrayList<Laptop> vratiPodatkeIzDatoteke() throws IOException, ClassNotFoundException {
-        return null;
+        JsonMapper mapper=new JsonMapper();
+        return mapper.readValue(file, new TypeReference<ArrayList<Laptop>>() {
+        });
     }
 }
